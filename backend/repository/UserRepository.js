fd1d6239f9d066ab1bee5fbbd7725604f4269576
Pamudi05 <113631350+Pamudi05@users.noreply.github.com>
2026-01-16 21:26:38 +0530

    register backend

--- backend/repository/UserRepository.js
+++ backend/repository/UserRepository.js
@@ -1,32 +1,27 @@
 import { pool } from "../db/dbConnection.js";
 import bcrypt from "bcrypt";
 
-export const create = async (email, name, phoneNumber, password) => {
-  const reg = await pool.getConnection();
+export const create = async (email, name, phoneNumber, password, res) => {
+  const db = await pool.getConnection();
 
-  const [existingEmail] = await reg.query(`SELECT id FROM users WHERE email=?`, [email]);
+  const [existingEmail] = await db.query(`SELECT * FROM users WHERE email=?`, [email]);
 
   if (existingEmail.length > 0) {
-    throw new Error ("Email Already Exists")
+    return res.status(409).json({message : "Email Already Exists"});
   }
 
   const hashPassword = await bcrypt.hash(password, 10);
 
   const Query = `INSERT INTO users(email, name, phoneNumber, password, roleId)
-        VALUES(?,?,?,?,(SELECT id FROM roles WHERE role = 'customer'))`;
+        VALUES(?,?,?,?,2)`;
 
   try {
-    const [result] = await reg.query(Query, [
-      email,
-      name,
-      phoneNumber,
-      hashPassword,
-    ]);
+    const [result] = await db.query(Query, [email,name,phoneNumber,hashPassword]);
     return result;
   } catch (error) {
     console.log(error);
     throw error;
   } finally {
-    reg.release();
+    db.release();
   }
 };
