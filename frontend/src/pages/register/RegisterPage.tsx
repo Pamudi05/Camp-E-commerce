657bf7994e7baa03bc77c1beab5c9d18a86d324f
Pamudi05 <113631350+Pamudi05@users.noreply.github.com>
2026-01-19 14:58:33 +0530

    register validations

--- frontend/src/pages/register/RegisterPage.tsx
+++ frontend/src/pages/register/RegisterPage.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./RegisterPage.css";
 import TextField from "../../components/input/TextField";
 import Button from "../../components/button/Button";
@@ -7,31 +7,111 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 const RegisterPage = () => {
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
   const [password, setPassword] = useState("");
 
+  const [emailError, setEmailError] = useState("");
+  const [nameError, setNameError] = useState("");
+  const [phoneError, setPhoneError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+
+  const [emailTouched, setEmailTouched] = useState(false);
+  const [nameTouched, setNameTouched] = useState(false);
+  const [phoneTouched, setPhoneTouched] = useState(false);
+  const [passwordTouched, setPasswordTouched] = useState(false);
+
+  const validateEmail = (email: string) => {
+    const emailRegex = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/;
+    return emailRegex.test(email);
+  };
+
+  const validateName = (name: string) => {
+    return name.trim().length > 0;
+  };
+
+  const validatePhone = (phoneNumber: string) => {
+    const phoneRegex = /^(?:\+94\s?7\d{8}|0\s*7\d{1}\s*\d{3}\s*\d{4})$/;
+    console.log(phoneNumber)
+    console.log(phoneRegex.test(phoneNumber))
+    return phoneRegex.test(phoneNumber);
+    
+  };
+
+  const validatePassword = (password: string) => {
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/;
+    return passwordRegex.test(password);
+  };
+
+  const handleEmailError = () => {
+    setEmailTouched(true);
+
+    if (!validateEmail(email)) {
+      setEmailError("Incorrect email. Please try again.");
+    } else {
+      setEmailError("");
+    }
+  };
+
+  const handlePhoneError = () => {
+    setPhoneTouched(true);
+
+    if (!validatePhone(phoneNumber)) {
+      setPhoneError("Should be correct phone number format");
+    } else {
+      setPhoneError("");
+    }
+  };
+
+  const handlePasswordError = () => {
+    setPasswordTouched(true);
+
+    if (!validatePassword(password)) {
+      setPasswordError("Password must be at least 8 characters.");
+    } else {
+      setPasswordError("");
+    }
+  };
+
   const handleRegister = async () => {
     try {
-      const response = await axios.post(
-        "http://localhost:5000/api/v1/users/create",
-        {
-          email,
-          name,
-          phoneNumber,
-          password,
-        }
-      );
+      setEmailTouched(true);
+      setNameTouched(true);
+      setPhoneTouched(true);
+      setPasswordTouched(true);
 
-      setEmail("");
-      setName("");
-      setPhoneNumber("");
-      setPassword("");
+      const isEmailValid = validateEmail(email);
+      const isNameValid = validateName(name);
+      const isPhoneValid = validatePhone(phoneNumber);
+      const isPasswordValid = validatePassword(password);
 
-      toast.success("User Registered Succefully");
-      return response.data;
+      setEmailError(isEmailValid ? "" : "Email is required");
+      setNameError(isNameValid ? "" : "Name is required");
+      setPhoneError(isPhoneValid ? "" : "Phone Number is required");
+      setPasswordError(isPasswordValid ? "" : "Password is required");
+
+      if (isEmailValid && isNameValid && isPhoneValid && isPasswordValid) {
+        const response = await axios.post(
+          "http://localhost:5000/api/v1/users/create",
+          {
+            email,
+            name,
+            phoneNumber,
+            password,
+          },
+        );
+
+        setEmail("");
+        setName("");
+        setPhoneNumber("");
+        setPassword("");
+
+        toast.success("User Registered Succefully");
+        navigate("/");
+        return response.data;
+      }
     } catch (error) {
       toast.error("Register  Failed");
       console.error(error);
@@ -51,28 +131,50 @@ const RegisterPage = () => {
               className="textfield"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
+              onBlur={handleEmailError}
             />
+            {emailTouched && emailError && (
+              <p className="error-text">{emailError}</p>
+            )}
+          </div>
+          <div className="textfile-box">
             <TextField
               type="text"
               placeholder="Name"
               className="textfield"
               value={name}
               onChange={(e) => setName(e.target.value)}
+              onBlur={() => setNameTouched(true)}
             />
+            {nameTouched && nameError && (
+              <p className="error-text">{nameError}</p>
+            )}
+          </div>
+          <div className="textfile-box">
             <TextField
-              type="number"
+              type="tel"
               placeholder="Contact Number"
               className="textfield"
               value={phoneNumber}
               onChange={(e) => setPhoneNumber(e.target.value)}
+              onBlur={handlePhoneError}
             />
+            {phoneTouched && phoneError && (
+              <p className="error-text">{phoneError}</p>
+            )}
+          </div>
+          <div className="textfile-box">
             <TextField
               type="password"
               placeholder="Password"
               className="textfield"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
+              onBlur={handlePasswordError}
             />
+            {passwordTouched && passwordError && (
+              <p className="error-text">{passwordError}</p>
+            )}
           </div>
           <div className="button-box">
             <Button
